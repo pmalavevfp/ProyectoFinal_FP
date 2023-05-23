6fbe86be262c920df419b3f6e9f9b906abc99c76
plmalave <plmalavev@gmail.com>
2023-05-23 20:28:21 +0200

    se agregaron metodos para los permisos y verificacion de token y se comprobo el paso de información de la notificacion a la activity receptora

--- app/src/main/java/com/plmv/finalworkpushnotif/MainActivity.java
+++ app/src/main/java/com/plmv/finalworkpushnotif/MainActivity.java
@@ -1,11 +1,19 @@
 package com.plmv.finalworkpushnotif;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -26,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         btn_receiveNotif= findViewById(R.id.btn_receiveNotif);
         btn_sendNotif=findViewById(R.id.btn_sendNotif);
+
+        askNotificationPermission();
 
         btn_sendNotif.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,6 +58,59 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Se ha incluido al grupo callToRiders", Toast.LENGTH_LONG).show();
             }
         });
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+//                        String msg = getString(R.string.msg_token_fmt, token);
+//                        Log.d(TAG, msg);
+                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+
+                        Log.e("token","mi token es:"+token);
+                        //tokenphone=token;
+                    }
+                });
+
+
+    }
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher =
+            registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+                if (isGranted) {
+                    // FCM SDK (and your app) can post notifications.
+                } else {
+                    // TODO: Inform user that that your app will not show notifications.
+                    // Si el usuario niega el permiso, muestra un mensaje o realiza otra acción
+                    Toast.makeText(this, "El permiso de notificación ha sido denegado", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+    private void askNotificationPermission() {
+        // This is only necessary for API level >= 33 (TIRAMISU)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED) {
+                // FCM SDK (and your app) can post notifications.
+            } else if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                // TODO: display an educational UI explaining to the user the features that will be enabled
+                //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
+                //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
+                //       If the user selects "No thanks," allow the user to continue without notifications.
+            } else {
+                // Directly ask for the permission
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS);
+            }
+        }
     }
 
     private void changeActivity (int x){
--- app/src/main/java/com/plmv/finalworkpushnotif/ReceiveNotif.java
+++ app/src/main/java/com/plmv/finalworkpushnotif/ReceiveNotif.java
@@ -1,14 +1,29 @@
 package com.plmv.finalworkpushnotif;
 
+import static com.plmv.finalworkpushnotif.R.id.tvData;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ReceiveNotif extends AppCompatActivity {
+    String color;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_notif);
+
+        TextView tvData=findViewById(R.id.tvData);
+
+        Bundle datos = this.getIntent().getExtras();
+        if(datos != null) {
+            color = datos.getString("color");
+        }
+
+        tvData.setText(color);
+        Toast.makeText(this,color,Toast.LENGTH_LONG).show();
     }
 }
